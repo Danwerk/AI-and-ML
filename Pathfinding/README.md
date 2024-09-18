f1544a60c4014ee2eddeb06fcc07466cad280e9d
Danyil Kurbatov <dakurb@taltech.ee>
2024-09-18 20:17:01 +0300

    Implemented greedy and astar algorithms for path finding and tested on different board sizes. Described results in readme file.

---
+++ Pathfinding/README.md
@@ -0,0 +1,60 @@
+The results of BFS:
+```
+Cave300x300:
+BFS: Time to find solution: 178.43 ms
+BFS: Number of iterations: 47264
+BFS: Length of solution in steps: 554
+---------------------------------------
+
+Cave600x600:
+BFS: Time to find solution: 862.75 ms
+BFS: Number of iterations: 197806
+BFS: Length of solution in steps: 1247
+---------------------------------------
+
+Cave900x900:
+BFS: Time to find solution: 2021.83 ms
+BFS: Number of iterations: 450448
+BFS: Length of solution in steps: 1843
+```
+
+The results of Greedy:
+```
+Cave300x300:
+GREEDY: Time to find solution: 15.62 ms
+GREEDY: Number of iterations: 3358
+GREEDY: Length of solution in steps: 982
+---------------------------------------
+
+Cave600x600:
+GREEDY: Time to find solution: 37.90 ms
+GREEDY: Number of iterations: 6293
+GREEDY: Length of solution in steps: 1973
+---------------------------------------
+
+Cave900x900
+GREEDY: Time to find solution: 153.43 ms
+GREEDY: Number of iterations: 29496
+GREEDY: Length of solution in steps: 4129
+```
+
+The results of A*
+``` 
+Cave300x300
+A*: Time to find solution: 53.11 ms
+A*: Number of iterations: 8202
+A*: Length of solution in steps: 554
+---------------------------------------
+
+Cave600x600
+A*: Time to find solution: 316.84 ms
+A*: Number of iterations: 60472
+A*: Length of solution in steps: 1247
+---------------------------------------
+
+Cave900x900
+A*: Time to find solution: 600.09 ms
+A*: Number of iterations: 93999
+A*: Length of solution in steps: 1843
+
+```
--- Pathfinding/pathfinding.py
+++ Pathfinding/pathfinding.py
@@ -1,4 +1,6 @@
 import queue
+import time
+
 STARTING_POINT = 's'
 ENDING_POINT = 'D'
 
@@ -20,7 +22,6 @@ lava_map1 = [
     "                s              ",
 ]
 
-
 lava_map2 = [
     "     **********************    ",
     "   *******   D    **********   ",
@@ -38,6 +39,24 @@ lava_map2 = [
     "                               ",
     "                s              ",
 ]
+
+
+def reconstruct_path(came_from, start, end):
+    """Reconstructs the path from start to end by following the came_from dictionary."""
+    current = end
+    path = []
+    while current != start:  # Traverse back from the end to the start
+        path.append(current)
+        current = came_from[current]
+    path.append(start)  # Don't forget to add the start position
+    path.reverse()  # We reversed the path so it's from start to end
+    return path
+
+
+def heuristic(goal: tuple, next: tuple):
+    # Manhattan distance on a square grid
+    return abs(goal[0] - next[0]) + abs(goal[1] - next[1])
+
 
 # Function that calculates the location of provided character. In our case we have start and end points.
 def get_char_on_lava_map(char, lava_map):
@@ -66,28 +85,145 @@ def bfs(lava_map):
     end_coords = get_char_on_lava_map(ENDING_POINT, lava_map)
 
     frontier = queue.Queue()
+
     frontier.put(start_coords)
-    came_from = {}
+    came_from = dict()
     came_from[start_coords] = None
 
+    start_time = time.time()
+    iterations = 0
+
     while not frontier.empty():
+        iterations += 1
         current = frontier.get()
 
         # If diamond reached then end searching and current is our search result
         if current == end_coords:
             break
-        print(current)
         row, col = current
 
         neighbors = [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
-        print(neighbors)
         for move_row, move_col in neighbors:
 
             if can_move(lava_map, move_row, move_col) and (move_row, move_col) not in came_from:
                 frontier.put((move_row, move_col))
                 came_from[(move_row, move_col)] = current
 
+    path = reconstruct_path(came_from, start_coords, end_coords)
+    end_time = time.time()
+    result_time = (end_time - start_time) * 1000
+    # Number of steps from start to end
+    path_length = len(path) - 1
+
+    print(f"BFS: Time to find solution: {result_time:.2f} ms")
+    print(f"BFS: Number of iterations: {iterations}")
+    print(f"BFS: Length of solution in steps: {path_length}")
+
+
+def greedy(lava_map):
+    start_coords = get_char_on_lava_map(STARTING_POINT, lava_map)
+    end_coords = get_char_on_lava_map(ENDING_POINT, lava_map)
+
+    frontier = queue.PriorityQueue()
+    frontier.put((0, start_coords))
+    came_from = dict()
+    came_from[start_coords] = None
+
+    # Measure the time and iterations
+    start_time = time.time()
+    iterations = 0
+
+    while not frontier.empty():
+        iterations += 1
+        current = frontier.get()[1]
+
+        # If diamond reached then end searching and current is our search result
+        if current == end_coords:
+            break
+        row, col = current
+
+        neighbors = [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
+        for move_row, move_col in neighbors:
+
+            if can_move(lava_map, move_row, move_col) and (move_row, move_col) not in came_from:
+                priority = heuristic(end_coords, (move_row, move_col))
+                frontier.put((priority, (move_row, move_col)))
+                came_from[(move_row, move_col)] = current
+
+    path = reconstruct_path(came_from, start_coords, end_coords)
+    end_time = time.time()
+    result_time = (end_time - start_time) * 1000
+    # Number of steps from start to end
+    path_length = len(path) - 1
+
+    print(f"GREEDY: Time to find solution: {result_time:.2f} ms")
+    print(f"GREEDY: Number of iterations: {iterations}")
+    print(f"GREEDY: Length of solution in steps: {path_length}")
+
+
+def a_star(lava_map):
+    start_coords = get_char_on_lava_map(STARTING_POINT, lava_map)
+    end_coords = get_char_on_lava_map(ENDING_POINT, lava_map)
+
+    frontier = queue.PriorityQueue()
+    frontier.put((0, start_coords))
+    came_from = dict()
+    came_from[start_coords] = None
+    cost_so_far = dict()
+    cost_so_far[start_coords] = 0
+
+    # Measure the time and iterations
+    start_time = time.time()
+    iterations = 0
+
+    while not frontier.empty():
+        iterations += 1
+        current = frontier.get()[1]
+
+        # If diamond reached then end searching and current is our search result
+        if current == end_coords:
+            break
+        row, col = current
+
+        neighbors = [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
+        for move_row, move_col in neighbors:
+            new_cost = cost_so_far[current] + 1
+
+            if can_move(lava_map, move_row, move_col) and (
+                    (move_row, move_col) not in cost_so_far or new_cost < cost_so_far[(move_row, move_col)]):
+                cost_so_far[(move_row, move_col)] = new_cost
+                priority = heuristic(end_coords, (move_row, move_col)) + new_cost
+                frontier.put((priority, (move_row, move_col)))
+                came_from[(move_row, move_col)] = current
+
+    path = reconstruct_path(came_from, start_coords, end_coords)
+    end_time = time.time()
+    result_time = (end_time - start_time) * 1000
+    # Number of steps from start to end
+    path_length = len(path) - 1
+
+    print(f"A*: Time to find solution: {result_time:.2f} ms")
+    print(f"A*: Number of iterations: {iterations}")
+    print(f"A*: Length of solution in steps: {path_length}")
+    # print_solution(lava_map, path)
+
+
+def print_solution(lava_map, path):
+    # Convert the map into a list of lists so we can modify it
+    map_copy = [list(row) for row in lava_map]
+
+    # Go through the path and replace the characters with '.'
+    for row, col in path[1:-1]:  # Exclude start and end points
+        map_copy[row][col] = '.'
+
+    # Convert the map back to string rows and print the map
+    for row in map_copy:
+        print(''.join(row))
 
 
 if __name__ == '__main__':
-    bfs(lava_map1)
+    with open("cave900x900") as f:
+        map_data = [l.strip() for l in f.readlines() if len(l) > 1]
+    # bfs(map_data)
+    # greedy(map_data)
+    a_star(map_data)
